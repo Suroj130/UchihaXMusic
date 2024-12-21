da2c3400f3ecee5960186783aa871f6da20d2a42
surojbera1000 <sandipberaind@gmail.com>
2024-12-21 12:08:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -89,10 +89,10 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://files.catbox.moe/wocluq.jpg"
+    "START_IMG_URL", "https://files.catbox.moe/gzygbt.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://te.legra.ph/file/4f59fb748e1990acfa297.jpg"
+    "PING_IMG_URL", "https://files.catbox.moe/2xadjk.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/14eb59ea7d31229d8d751.jpg"
 STATS_IMG_URL = "https://te.legra.ph/file/4310ea5f523520b2b765b.jpg"
