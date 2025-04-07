518f76cb1debb16252fb381765ab4a4df3b7152e
Suroj130 <sandipbera.ind@gmail.com>
2025-04-07 10:23:09 +0530

    Update sessiongen.py

--- BrandrdXMusic/plugins/tools/sessiongen.py
+++ BrandrdXMusic/plugins/tools/sessiongen.py
@@ -1,23 +1,8 @@
-import asyncio
-from pyrogram import Client, __version__ as ver
+from pyrogram import Client
 
-API_ID = input("\nআপনার API ID দিন:\n> ")
-API_HASH = input("\nআপনার API HASH দিন:\n> ")
+api_id = int(input("Enter your API ID: "))
+api_hash = input("Enter your API HASH: ")
 
-print("\n\nটেলিগ্রাম অ্যাকাউন্টের সাথে যুক্ত ফোন নম্বর দিন যখন জিজ্ঞাসা করা হবে।\n\n")
-
-fallen = Client("Fallen", api_id=API_ID, api_hash=API_HASH, in_memory=True)
-
-async def main():
-    await fallen.start()
-    sess = await fallen.export_session_string()
-    txt = f"এটি আপনার Pyrogram {ver} স্ট্রিং সেশন\n\n<code>{sess}</code>\n\nএটি কারো সাথে শেয়ার করবেন না।\n@BRANDED_WORLD-এ যোগ দিতে ভুলবেন না।"
-    await fallen.send_message("me", txt)
-    print(f"এটি আপনার Pyrogram {ver} স্ট্রিং সেশন\n{sess}\nকপি করতে ডাবল ক্লিক করুন।")
-
-if __name__ == "__main__":
-    try:
-        asyncio.run(main())
-    except RuntimeError:
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(main())
+with Client("my_session", api_id=api_id, api_hash=api_hash) as app:
+    print("Your session string:")
+    print(app.export_session_string())
