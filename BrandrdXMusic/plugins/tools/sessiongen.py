a22db3465139d687a2fc31cbb403232182b3516c
Suroj130 <sandipbera.ind@gmail.com>
2025-04-07 10:28:36 +0530

    Update sessiongen.py

--- BrandrdXMusic/plugins/tools/sessiongen.py
+++ BrandrdXMusic/plugins/tools/sessiongen.py
@@ -1,8 +1,14 @@
+import asyncio
 from pyrogram import Client
 
 api_id = int(input("Enter your API ID: "))
 api_hash = input("Enter your API HASH: ")
 
-with Client("my_session", api_id=api_id, api_hash=api_hash) as app:
-    print("Your session string:")
-    print(app.export_session_string())
+
+async def main():
+    async with Client("my_session", api_id=api_id, api_hash=api_hash) as app:
+        session_string = await app.export_session_string()
+        print("Your session string:")
+        print(session_string)
+
+asyncio.run(main())
