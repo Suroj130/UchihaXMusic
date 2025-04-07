211f08ed24473d005c0b6dbaba0c33ebe0127240
Suroj130 <sandipbera.ind@gmail.com>
2025-04-07 10:07:05 +0530

    Update sessiongen.py

--- BrandrdXMusic/plugins/tools/sessiongen.py
+++ BrandrdXMusic/plugins/tools/sessiongen.py
@@ -1,22 +1,23 @@
 import asyncio
-
 from pyrogram import Client, __version__ as ver
 
+API_ID = input("\nআপনার API ID দিন:\n> ")
+API_HASH = input("\nআপনার API HASH দিন:\n> ")
 
-API_ID = input("\nEnter Your API ID :\n > ")
-API_HASH = input("\nEnter Your API HASH :\n > ")
-
-print("\n\nEnter the phone number associated with your telegram account when asked.\n\n")
+print("\n\nটেলিগ্রাম অ্যাকাউন্টের সাথে যুক্ত ফোন নম্বর দিন যখন জিজ্ঞাসা করা হবে।\n\n")
 
 fallen = Client("Fallen", api_id=API_ID, api_hash=API_HASH, in_memory=True)
-
 
 async def main():
     await fallen.start()
     sess = await fallen.export_session_string()
-    txt = f"Here is your Pyrogram {ver} String Session\n\n<code>{sess}</code>\n\nDon't share it with anyone.\nDon't forget to join @BRANDED_WORLD"
-    ok = await fallen.send_message("me", txt)
-    print(f"Here is your Pyrogram {ver} String Session\n{sess}\nDouble click to copy.") 
+    txt = f"এটি আপনার Pyrogram {ver} স্ট্রিং সেশন\n\n<code>{sess}</code>\n\nএটি কারো সাথে শেয়ার করবেন না।\n@BRANDED_WORLD-এ যোগ দিতে ভুলবেন না।"
+    await fallen.send_message("me", txt)
+    print(f"এটি আপনার Pyrogram {ver} স্ট্রিং সেশন\n{sess}\nকপি করতে ডাবল ক্লিক করুন।")
 
-
-asyncio.run(main())
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except RuntimeError:
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(main())
