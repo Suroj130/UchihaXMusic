4cf1ccc4557a0b7d76e0ef55b89bece503a5d604
Suroj130 <sandipbera.ind@gmail.com>
2025-04-09 10:23:49 +0530

    Update buttons.py

--- BrandrdXMusic/utils/stuffs/buttons.py
+++ BrandrdXMusic/utils/stuffs/buttons.py
@@ -12,7 +12,6 @@ class BUTTONS(object):
     [InlineKeyboardButton("Sᴏᴜʀᴄᴇ", callback_data="mplus HELP_Source"),
     InlineKeyboardButton("Tʀᴜᴛʜ-ᗪᴀʀᴇ", callback_data="mplus HELP_TD"),InlineKeyboardButton("Qᴜɪᴢ", callback_data="mplus HELP_Quiz")], 
     [InlineKeyboardButton("ᴛᴛs", callback_data="mplus HELP_TTS"),
-InlineKeyboardButton("Shayari", callback_data="mplus HELP_Shayari")],
     InlineKeyboardButton("Rᴀᴅɪᴏ", callback_data="mplus HELP_Radio"),InlineKeyboardButton("ǫᴜᴏᴛʟʏ", callback_data="mplus HELP_Q")],          
     [InlineKeyboardButton("◁", callback_data=f"settings_back_helper"),
      InlineKeyboardButton("↻ ʙᴀᴄᴋ ↻", callback_data=f"mbot_cb"), 
